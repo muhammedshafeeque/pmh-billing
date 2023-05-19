58d987089556522d199efb4e80f9b46d42c9b100
Muhammed Shafeeque P <shafeequekkv95@gmail.com>
2023-05-19 12:40:26 +0530

    updated

---
+++ server/Models/CotegoryModal.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+const cateGoryModal = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    code: { type: String, require: true, unique: true },
+    racks: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "rack", required: true },
+    ],
+  },
+  {
+    timestaps: true,
+  }
+);
+export const CateGory = mongoose.model("category", cateGoryModal);
