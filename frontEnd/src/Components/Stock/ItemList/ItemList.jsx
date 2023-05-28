c30155cd4adb588a4277c63aa68ae9debaf82c7c
Muhammed Shafeeque P <shafeequekkv95@gmail.com>
2023-05-28 18:37:41 +0530

    updated

--- frontEnd/src/Components/Stock/ItemList/ItemList.jsx
+++ frontEnd/src/Components/Stock/ItemList/ItemList.jsx
@@ -1,8 +1,31 @@
+import { Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
 import React from 'react'
-
+import './ItemList.scss'
 function ItemList() {
   return (
-    <div>ItemList</div>
+    <div>
+        <Table size="sm">
+            <div className="search_area">
+                
+            </div>
+          <Thead>
+            <Tr>
+              <Th>Name</Th>
+              <Th>code</Th>
+              <Th>Section</Th>
+              <Th>Actions</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+            </Tr>
+          </Tbody>
+        </Table>
+    </div>
   )
 }
 
--- frontEnd/src/Components/Stock/RackList/RackList.jsx
+++ frontEnd/src/Components/Stock/RackList/RackList.jsx
@@ -10,42 +10,42 @@ import {
 } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import CreateRack from "../CreateRack/CreateRack";
-import axios from '../../../Axios/axios'
+import axios from "../../../Axios/axios";
 import DeleteModal from "../../Misc/DeleteModal/DeleteModal";
 function RackList() {
-  const toast=useToast()
-  const [reload,setReload]=useState()
-  const [racks,setRacks]=useState([])
-  const handleSearch=async()=>{
-    let {data}=await axios.get("/stock/rack")
-    setRacks(data)
-  }
-  useEffect(()=>{
-    handleSearch()
-  },[reload])
-  const handleDelete=async(item)=>{
+  const toast = useToast();
+  const [reload, setReload] = useState();
+  const [racks, setRacks] = useState([]);
+  const handleSearch = async () => {
+    let { data } = await axios.get("/stock/rack");
+    setRacks(data);
+  };
+  useEffect(() => {
+    handleSearch();
+  }, [reload]);
+  const handleDelete = async (item) => {
     try {
-      let res= await axios.delete(`stock/rack/${item._id}`)
-       setReload(res)
-       toast({
-         title: 'Success',
-         description: 'Item Removed Successfully ',
-         status: "success",
-         duration: 5000,
-         isClosable: true,
-         position: "top-right",
-       });
-     } catch (error) {
-       toast({
-         title: 'Failed',
-         description: error.response.data,
-         status: "error",
-         duration: 5000,
-         isClosable: true,
-         position: "top-right",
-       });
-     };
-  }
+      let res = await axios.delete(`stock/rack/${item._id}`);
+      setReload(res);
+      toast({
+        title: "Success",
+        description: "Item Removed Successfully ",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "top-right",
+      });
+    } catch (error) {
+      toast({
+        title: "Failed",
+        description: error.response.data,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "top-right",
+      });
+    }
+  };
   return (
     <div style={{ marginTop: "3rem" }}>
       <CreateRack setFlage={setReload} />
@@ -55,32 +55,34 @@ function RackList() {
             <Tr>
               <Th>Name</Th>
               <Th>code</Th>
-              <Th >Section</Th>
+              <Th>Section</Th>
               <Th>Actions</Th>
             </Tr>
           </Thead>
           <Tbody>
-            {racks.map((item)=>{
-              return <Tr key={item._id}>
-              <Td>{item.name}</Td>
-              <Td>{item.code}</Td>
-              <Td >{item.section.name}</Td>
-              <Td display={"flex"} fontSize={'20px'}>
+            {racks.map((item) => {
+              return (
+                <Tr key={item._id}>
+                  <Td>{item.name}</Td>
+                  <Td>{item.code}</Td>
+                  <Td>{item.section.name}</Td>
+                  <Td display={"flex"} fontSize={"20px"}>
                     {" "}
-                    <span  style={{color:'red',cursor:"pointer"}}>
-                      <DeleteModal handleDelete={()=>handleDelete(item)}/>
-                    
+                    <span style={{ color: "red", cursor: "pointer" }}>
+                      <DeleteModal handleDelete={() => handleDelete(item)} />
                     </span>
                     <span></span>
-                    <span style={{color:'blue',cursor:'pointer'}}>
-                      <CreateRack update={true} doc={item} setFlage={setReload}/>
-             
+                    <span style={{ color: "blue", cursor: "pointer" }}>
+                      <CreateRack
+                        update={true}
+                        doc={item}
+                        setFlage={setReload}
+                      />
                     </span>{" "}
                   </Td>
-            </Tr>
+                </Tr>
+              );
             })}
-            
-            
           </Tbody>
         </Table>
       </TableContainer>
