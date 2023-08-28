8ecec11faac1c14881a3c3f5e8301424d741d3f7
Muhammed shafeeque P <shafeequkkv95@gmail.com>
2023-08-28 19:30:48 +0530

    section create complerated

--- pmh/src/Components/CreateAndUpdateSection/CreateAndUpdateSection.jsx
+++ pmh/src/Components/CreateAndUpdateSection/CreateAndUpdateSection.jsx
@@ -1,27 +1,52 @@
 import React from "react";
 import { Button, Form, Row } from "react-bootstrap";
-
+import { useForm } from "react-hook-form";
+import axios from "../../Api/Axios";
+import { mainEndPoint } from "../../Constants/ApiConstants/mainRoutes";
+import { configEndPoints } from "../../Constants/ApiConstants/config";
+import { useAlert } from "react-alert";
+import { Stor } from "../../Context/BillerContext";
 function CreateAndUpdateSection(props) {
+  const { register, handleSubmit } = useForm();
+  const alert = useAlert();
+  const { setBlockUi } = Stor();
+  const onSubmit = (value) => {
+    setBlockUi(true);
+    axios
+      .post(mainEndPoint.STOCK + configEndPoints.SECTION, value)
+      .then(({ data }) => {
+        alert.success("Section Created Successfully");
+        setBlockUi(false);
+        props.onHide();
+      })
+      .catch((err) => {
+        alert.error(err.response.data.message);
+        setBlockUi(false);
+      });
+  };
   return (
     <div className="p-4">
-      <Form>
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <Row>
           <Form.Group controlId="sectionName">
             <Form.Label>Section Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter section name" />
+            <Form.Control
+              type="text"
+              {...register("name", { required: true })}
+              placeholder="Enter section name"
+            />
           </Form.Group>
           <Form.Group controlId="sectionCode">
             <Form.Label>Section Code</Form.Label>
-            <Form.Control type="text" placeholder="Enter section code" />
+            <Form.Control
+              {...register("code", { required: true })}
+              type="text"
+              placeholder="Enter section code"
+            />
           </Form.Group>
         </Row>
         <div style={{ display: "flex", justifyContent: "flex-end" }}>
-          <Button
-            variant="primary"
-            onClick={props.onHide}
-            className="mt-4"
-            type="submit"
-          >
+          <Button variant="primary" className="mt-4" type="submit">
             Save
           </Button>
         </div>
--- pmh/src/Modules/Shop/Racks.jsx
+++ pmh/src/Modules/Shop/Racks.jsx
@@ -1,9 +1,95 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import SectionAutoCompleat from "../../Components/AutoCompleat/SectionAutoCompleat";
+import axios from "../../Api/Axios";
+import { mainEndPoint } from "../../Constants/ApiConstants/mainRoutes";
+import { configEndPoints } from "../../Constants/ApiConstants/config";
+import { commonFilters } from "../../Constants/ApiConstants/apiFilters";
+import { ApiDefaultValues } from "../../Constants/ApiConstants/ApiDefaultFilterValues";
+import { useAlert } from "react-alert";
+import { Stor } from "../../Context/BillerContext";
+import CreateAndUpdateSection from "../../Components/CreateAndUpdateSection/CreateAndUpdateSection";
+import Button from "react-bootstrap/esm/Button";
+import ModalComponent from "../../Components/Modal/Modal";
 function Racks() {
+  const [section, setSection] = useState();
+  const [results, setResults] = useState([]);
+  const alert = useAlert();
+  const { setBlockUi } = Stor();
+  const [modal, setModal] = useState(false);
+  useEffect(() => {
+    setBlockUi(true);
+
+    axios
+      .get(
+        mainEndPoint.STOCK +
+          configEndPoints.SECTION +
+          "?" +
+          commonFilters.LIMIT +
+          ApiDefaultValues.LIMIT +
+          `${section ? "&" + commonFilters.ID + section._id : ""}`
+      )
+      .then(({ data }) => {
+        setBlockUi(false);
+        setResults(data);
+      })
+      .catch((err) => {
+        setBlockUi(false);
+        alert.error(err.response.error);
+      });
+  }, [section, setBlockUi, alert]);
+
+  const handleModal = () => {
+    if (modal) {
+      setModal(false);
+      setSection([]);
+    } else {
+      setModal(true);
+    }
+  };
   return (
-    <div>Racks</div>
-  )
+    <div className="col-md-12">
+      <h2 className="page_header">Racks</h2>
+      <div className=" col-md-12 mt-5">
+        <h5>Search Rack</h5>
+      </div>
+      <div className="row col-md-12">
+        <div className="col-md-10">
+          <div className="col-md-3">
+            <SectionAutoCompleat setSection={setSection} />
+          </div>
+        </div>
+        <div className="col-md-2">
+          <Button onClick={handleModal}>Create Rack</Button>
+        </div>
+      </div>
+      <ModalComponent show={modal} popup_head='Create Section' >
+        <CreateAndUpdateSection onHide={() => handleModal()} />
+      </ModalComponent>
+
+      <table className="table table-bordered mt-4">
+        <thead>
+          <tr className="thead-light">
+            <th>Name</th>
+            <th>Code</th>
+            <th>Section</th>
+            <th>Total Investment</th>
+          </tr>
+        </thead>
+        <tbody>
+          {results.map((item) => {
+            return (
+              <tr key={item._id}>
+                <td>{item.name}</td>
+                <td>{item.code}</td>
+                <td>{item.numberOfRacks}</td>
+                <td></td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Racks
