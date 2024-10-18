d786bf766238726b2b0357d3c0e3312ad1a1c438
Muhammed Shafeeque P <shafeequekkv95@gmail.com>
2024-10-18 10:27:21 +0530

    account creation modified

--- pmh-store/src/Components/Accounts/CreateAndUpdateAccount.tsx
+++ pmh-store/src/Components/Accounts/CreateAndUpdateAccount.tsx
@@ -1,37 +1,58 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { Form, Button } from "react-bootstrap";
 import axios from "../../Api/Api";
 import { useLoading } from "../../Contexts/LoaderContext";
+import { FaSave, FaTimes } from "react-icons/fa";
 
+interface Account {
+  _id?: string;
+  name: string;
+  // Add other account properties here
+}
 
-const CreateAndUpdateAccount: React.FC<PopupChildeProp> = ({ handleClose }) => {
+interface CreateAndUpdateAccountProps extends PopupChildeProp {
+  accountToEdit?: Account | null;
+}
+
+const CreateAndUpdateAccount: React.FC<CreateAndUpdateAccountProps> = ({ handleClose, accountToEdit }) => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } :any= useForm<any>();
+    reset,
+  } = useForm<Account>();
   const { setLoadingState } = useLoading();
-  const onSubmit: SubmitHandler<any> = async (
-    data: any
-  ) => {
+
+  useEffect(() => {
+    if (accountToEdit) {
+      reset(accountToEdit);
+    }
+  }, [accountToEdit, reset]);
+
+  const onSubmit: SubmitHandler<Account> = async (data: Account) => {
     try {
       setLoadingState(true);
-      await axios.post("accounts/account", data);
-      handleClose()
+      if (accountToEdit) {
+        await axios.patch(`accounts/account/${accountToEdit._id}`, data);
+      } else {
+        await axios.post("accounts/account", data);
+      }
+      handleClose();
     } catch (error) {
+      console.error("Error submitting account:", error);
     } finally {
       setLoadingState(false);
-    } 
+    }
   };
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
-      <Form.Group controlId="formSectionName">
+      <Form.Group controlId="formAccountName">
         <Form.Label>Name</Form.Label>
         <Form.Control
           type="text"
-          placeholder="Enter section name"
+          placeholder="Enter account name"
           {...register("name", { required: "Name is required" })}
           isInvalid={!!errors.name}
         />
@@ -39,9 +60,17 @@ const CreateAndUpdateAccount: React.FC<PopupChildeProp> = ({ handleClose }) => {
           {errors.name?.message}
         </Form.Control.Feedback>
       </Form.Group>
-      <Button variant="primary" type="submit" className="mt-3">
-        Submit
-      </Button>
+
+      {/* Add more form fields for other account properties here */}
+
+      <div className="modal-footer">
+        <Button variant="secondary" onClick={handleClose} className="me-2">
+          <FaTimes /> Cancel
+        </Button>
+        <Button variant="primary" type="submit">
+          <FaSave /> {accountToEdit ? 'Update' : 'Save'} Account
+        </Button>
+      </div>
     </Form>
   );
 };
